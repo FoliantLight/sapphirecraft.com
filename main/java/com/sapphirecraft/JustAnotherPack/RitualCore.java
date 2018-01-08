5f04db00d87b77deb51135c1145504e4933bd918
Foliant Ligth <foliant.light@yandex.ru>
2018-01-09 01:15:59 +0300

    Ritual is coming!

--- main/java/com/sapphirecraft/JustAnotherPack/RitualCore.java
+++ main/java/com/sapphirecraft/JustAnotherPack/RitualCore.java
@@ -4,7 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 
-public class ritualCore {
+public class RitualCore {
 	public void ritualOreToOre(World world, int x, int y, int z, Block block)
 	{
 		if(getCountOfBlock(world,x,y,z,block) == 9)
