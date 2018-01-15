177e04bc5bc2bf2c200529b065e4841c973ae49b
ALMIGHTILYPC\angel <angelmilnight@gmail.com>
2018-01-15 13:47:06 +0200

    А кто тут молодец? :)

--- main/java/com/sapphirecraft/JustAnotherPack/Main.java
+++ main/java/com/sapphirecraft/JustAnotherPack/Main.java
@@ -3,8 +3,8 @@ package com.sapphirecraft.JustAnotherPack;
 import com.sapphirecraft.JustAnotherPack.Tabs.ModTabs;
 import com.sapphirecraft.JustAnotherPack.blocks.ModBlocks;
 import com.sapphirecraft.JustAnotherPack.items.ModItems;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.WeightedRandomChestContent;
@@ -18,10 +18,14 @@ public class Main {
     public static final String MODID = "JAS";
     public static final String NAME = "Just Another Sapphirepack";
     public static final String VERSION = "0.0.4";
+	private EventHandler events = new EventHandler();
 	
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
+		FMLCommonHandler.instance().bus().register(events);
+		MinecraftForge.EVENT_BUS.register(events);
+
 		ModTabs.init();
 		ModBlocks.init();
 		ModItems.init();
@@ -31,11 +35,4 @@ public class Main {
 		ChestGenHooks.addItem(ChestGenHooks.DUNGEON_CHEST, new WeightedRandomChestContent(
 				new ItemStack(ModItems.itemRuby), 1, 3, 3));
 	}
-
-	@Mod.EventHandler
-    public void init(FMLInitializationEvent e)
-    {
-    	MinecraftForge.EVENT_BUS.register(new EventHandler());
-    }
-    
 }
